76d1471f5032050619cc2ba6c7e6f0ecfb791801
Giulia Vallente <giuliagv@al.insper.edu.br>
2023-04-20 15:57:11 -0300

    Movimenta personagem - GV

---
+++ classes.py
@@ -0,0 +1,48 @@
+import pygame
+
+class Personagens(pygame.sprite.Sprite):
+    COLOR = (255, 0, 0)
+    GRAVIDADE = 1
+
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.direcao = 'esquerda'
+        self.animation_count = 0
+        self.tempo_queda = 0
+    
+    def movimenta(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def movimenta_esquerda(self, vel):
+        self.x_vel = -vel
+        if self.direcao != 'esquerda':
+            self.direcao = 'esquerda'
+            self.animation_count = 0
+    
+    def movimenta_direita(self, vel):
+        self.x_vel = vel
+        if self.direcao != 'direita':
+            self.direcao = 'direita'
+            self.animation_count = 0
+    
+    def loop(self, fps):
+        self.movimenta(self.x_vel, self.y_vel)
+    
+    def desenha(self, window):
+        pygame.draw.rect(window, self.COLOR, self.rect)
+
+    
+
+
+class Objetos(pygame.sprite.Sprite):
+    pass
+        
+    
+    
+    
+
+
+    
--- funcoes.py
+++ funcoes.py
@@ -1,23 +1,46 @@
 import pygame
+import classes
 
-def inicializa():
-    pygame.init()
-    window = pygame.display.set_mode((1280, 720))
-    pygame.display.set_caption("Hogwarts Scape")
+pygame.init()
+pygame.display.set_caption("Hogwarts Scape")
+window = pygame.display.set_mode((1280, 720))
 
-    return window
+FPS = 60
+VEL_JOGADOR = 5
 
-def atualiza_estado():
-    for event in pygame.event.get():
+def gera_fundo():
+    fundo = pygame.transform.scale(pygame.image.load('imagens/masmorra_real.jpg'), (1280,720))
+    return fundo
 
-        if event.type == pygame.QUIT:
-            return False
-    
-    return True
+def desenha(window, fundo, harry):
+    window.blit(fundo, (0, 0))
+    harry.desenha(window)
+    pygame.display.update()
+ 
+def detecta_movimento(jogador):
+    chaves = pygame.key.get_pressed()
+    jogador.x_vel = 0
+    if chaves[pygame.K_LEFT]:
+        jogador.movimenta_esquerda(VEL_JOGADOR)
+    if chaves[pygame.K_RIGHT]:
+        jogador.movimenta_direita(VEL_JOGADOR)
 
-def desenha(window):
-    window.fill((0, 0, 0))
+def main(window):
+    clock = pygame.time.Clock()
+    harry = classes.Personagens(100, 100, 50, 50)
 
-def game_loop(window):
-    while atualiza_estado():
-        desenha(window)
+    jogo = True
+    while jogo: 
+        clock.tick(FPS)
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                jogo = False
+                break  
+            
+            harry.loop(FPS)
+            detecta_movimento(harry)
+            desenha(window, gera_fundo(), harry)
+
+if __name__ == '__main__':
+    main(window)
