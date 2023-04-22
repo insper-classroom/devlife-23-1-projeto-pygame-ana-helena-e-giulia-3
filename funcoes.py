3249d229c2165870e5b099fa8c42ee256a132a0e
Ana Helena <anahc3@al.insper.edu.br>
2023-04-22 18:25:25 -0300

    tela inicial + tela instruções implementadas - AH

--- funcoes.py
+++ funcoes.py
@@ -7,12 +7,18 @@ def inicializa():
     window = pygame.display.set_mode((1280, 720))
 
     assets = {}
-    state = {}
+    state = {
+        'tela_inicio': True,
+        'tela_instrucoes': False,
+        'tela_jogo': False
+    }
 
     return window, assets, state
 
+
 FPS = 60
 VEL_JOGADOR = 5
+
 
 def gera_fundo():
     altura = 144
@@ -47,20 +53,70 @@ def carrega_sprite(direcao=False):
     
     return todas_sprites
 
+
 def carrega_bloco():
     imagem = (pygame.transform.scale(pygame.image.load('imagens/terreno.jpg'), (72, 72)))
     return imagem
                
 
-def desenha(window, background, bg_image, harry, objetos):
-    for tile in background:
-        window.blit(bg_image, tile)
-    
-    for objeto in objetos:
-        objeto.desenha(window)
+def desenha(window, background, bg_image, harry, objetos, state):
+    window.fill((0, 0, 0))
 
-    harry.desenha(window)
+    if state['tela_inicio']:
+        imagem = pygame.image.load('imagens/fundo_inicio.jpg')
+        fundo = pygame.transform.scale(imagem, (1280, 720))
+        window.blit(fundo, (0, 0))
+
+        mouse_pos = pygame.mouse.get_pos()
+
+        fonte_titulo = pygame.font.Font('docs/font/WizardWorldSimplified-Kxr7.ttf', 100)
+        titulo_jogo = fonte_titulo.render('Hogwarts Scape', True, ((211, 177, 110)))
+        window.blit(titulo_jogo, (220, 130))
+            
+        fonte_text = pygame.font.Font('docs/font/WizardWorldSimplified-Kxr7.ttf', 60)
+
+        play = fonte_text.render('Play', True, (174, 139, 71))
+        play_rect = play.get_rect(center=(1280/2, 720/2))
+        window.blit(play, play_rect)
+
+        if play_rect.collidepoint((mouse_pos)) and pygame.mouse.get_pressed()[0]:
+            state['tela_inicio'] = False
+            state['tela_jogo'] = True
+
+        instrucoes = fonte_text.render('Instruções', True, (174, 139, 71))
+        instrucoes_rect = instrucoes.get_rect(center=(1280/2, 500))
+        window.blit(instrucoes, instrucoes_rect)
+        
+        if instrucoes_rect.collidepoint((mouse_pos)) and pygame.mouse.get_pressed()[0]:
+            state['tela_inicio'] = False
+            state['tela_instrucoes'] = True
+
+    if state['tela_instrucoes']:
+        window.fill((230, 201, 143))
+
+        mouse_pos = pygame.mouse.get_pos()
+
+        fonte_text = pygame.font.Font('docs/font/WizardWorldSimplified-Kxr7.ttf', 60)
+
+        play = fonte_text.render('Play', True, (174, 139, 71))
+        play_rect = play.get_rect(center=(1280/2, 720/2))
+        window.blit(play, play_rect)
+
+        if play_rect.collidepoint((mouse_pos)) and pygame.mouse.get_pressed()[0]:
+            state['tela_inicio'] = False
+            state['tela_jogo'] = True
+    
+    if state['tela_jogo']:
+        for tile in background:
+            window.blit(bg_image, tile)
+
+        for objeto in objetos:
+            objeto.desenha(window)
+
+        harry.desenha(window)
+
     pygame.display.update()
+
 
 def main(window, assets, state):
     clock = pygame.time.Clock()
@@ -88,9 +144,13 @@ def main(window, assets, state):
                     harry.movimenta_esquerda(0)
                 if event.key == pygame.K_RIGHT:
                     harry.movimenta_direita(0) 
-            
-        harry.loop(FPS)
-        desenha(window, background, bg_image, harry, chao)
+
+        if state['tela_jogo']:
+            harry.loop(FPS)
+            desenha(window, background, bg_image, harry, chao, state)
+        else:
+            desenha(window, background, bg_image, harry, chao, state)
+
 
 if __name__ == '__main__':
     window, assets, state = inicializa()
